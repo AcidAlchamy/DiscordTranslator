f154c540275764f1d8c1ec35ad08ff9b8c8c532b
AcidAlchamy <acidalchamy@yahoo.com>
2023-10-18 11:16:53 -0700

    Update files

--- script.js
+++ script.js
@@ -6,7 +6,24 @@ document.addEventListener("DOMContentLoaded", function() {
         
         const amount = document.getElementById("amount").value;
         const use = document.getElementById("use").value;
+        const paymentMethod = document.getElementById("paymentMethod").value;
 
-        // Perform donation action, such as API calls to payment gateway.
+        // Generate a Transaction ID here (Requires Backend Logic)
+        // let transactionID = generateTransactionID();
+
+        let redirectUrl = '';
+        
+        if (paymentMethod === 'paypal') {
+            redirectUrl = `https://paypal.me/AcidAlchamy?amount=${amount}`;
+        } else if (paymentMethod === 'cashapp') {
+            redirectUrl = `https://cash.app/$AcidAlchamy?amount=${amount}`;
+        } else if (paymentMethod === 'bitcoin') {
+            // Handle Bitcoin, probably show an alert or a modal with address
+            alert("Please send your bitcoin to the address. [Insert bitcoin address]");
+            return;
+        }
+        
+        // Redirect to the payment gateway
+        window.location.href = redirectUrl;
     });
 });
